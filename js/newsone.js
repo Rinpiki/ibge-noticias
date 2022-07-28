03f71956d5dcbd38b16669a2c1b8d0c883e03153
Marcos Pablo <rinpikibr@gmail.com>
2022-07-28 16:36:24 -0300

    api de img feita mas nao funcional

--- js/newsone.js
+++ js/newsone.js
@@ -1,26 +1,28 @@
-const titulo = document.querySelector('.titulo-card');
-const texto = document.querySelector('.texto-card');
-const data = document.querySelector('.data-card');
-
 export default async function ibgeApi() {
   const dados = await fetch(
     'http://servicodados.ibge.gov.br/api/v3/noticias/?tipo=release'
   );
+  const link = 'http://servicodados.ibge.gov.br/api/v3/noticias/?tipo=release';
   const ibge = await dados.json();
   console.log(ibge);
   const notA = ibge.items[0];
   const notB = ibge.items[1];
   const notC = ibge.items[2];
   const todasNot = [notA, notB, notC];
+  const imgg = JSON.parse(notA.imagens);
+  const img = imgg.image_intro;
+  const imagemFinal = await `${link}/${img}.443`;
+  console.log(imagemFinal);
+  console.log(img);
 
   todasNot.forEach((items) => {
     const notfeitas = items.titulo;
-    const introducao = items.introducao;
+    const introducao = items.introducao.slice(0, 110) + '...';
     const data_publicacao = items.data_publicacao.slice(0, 10);
     const card = document.createElement('div');
     card.innerHTML = `<div class="cards">
           <div class="img-card">
-            <img src="img/CENSO_CAC_home.jpg" alt="" />
+            <img src="${imagemFinal}" alt="" />
           </div>
           <div class="card-texto-container">
             <h3 class="titulo-card">
