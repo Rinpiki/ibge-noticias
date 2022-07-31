173f1e262b1c3b841020b6ef2fa2d54acfe9ddba
Marcos Pablo <rinpikibr@gmail.com>
2022-07-31 18:30:03 -0300

    add função para colocar imgs nos cards

--- js/newsone.js
+++ js/newsone.js
@@ -2,27 +2,41 @@ export default async function ibgeApi() {
   const dados = await fetch(
     'https://servicodados.ibge.gov.br/api/v3/noticias/?tipo=release'
   );
-  const link = 'https://servicodados.ibge.gov.br/api/v3/noticias/?tipo=release';
+
   const ibge = await dados.json();
   console.log(ibge);
   const notA = ibge.items[0];
   const notB = ibge.items[1];
   const notC = ibge.items[2];
   const todasNot = [notA, notB, notC];
-  const imgg = JSON.parse(notA.imagens);
-  const img = imgg.image_intro;
-  const imagemFinal = await `${link}/${img}.443`;
-  console.log(imagemFinal);
-  console.log(img);
+  const capaEconomicas = [
+    'img/economicasBitcoin.svg',
+    'img/economicasGod.svg',
+    'img/economicasInvestimentos.svg',
+    'img/economicasPizza.svg',
+    'img/economicasProjeção.svg',
+  ];
+
+  const capaSociais = [
+    'img/sociaisAmigos.svg',
+    'img/sociaisCoração.svg',
+    'img/sociaisMulher.svg',
+    'img/sociaisMusica.svg',
+    'img/sociaisProfessor.svg',
+    'img/sociaisProjeto.svg',
+  ];
 
   todasNot.forEach((items) => {
     const notfeitas = items.titulo.slice(0, 70) + '...';
     const introducao = items.introducao.slice(0, 110) + '...';
     const data_publicacao = items.data_publicacao.slice(0, 10);
-    const card = document.createElement('div');
-    card.innerHTML = `<div class="cards">
+    if (items.editorias == 'economicas') {
+      const numeroAleatorio = (Math.random() * 5).toFixed(0);
+      console.log(numeroAleatorio);
+      const card = document.createElement('div');
+      card.innerHTML = `<div class="cards">
           <div class="img-card">
-            <img src="img/pessoa.png" alt="" />
+            <img src="${capaEconomicas[numeroAleatorio]}" alt="" />
           </div>
           <div class="card-texto-container">
             <h3 class="titulo-card">
@@ -34,7 +48,26 @@ export default async function ibgeApi() {
           </div>
           <span class="data-card">${data_publicacao}</span>
         </div>`;
-    console.log(card);
-    document.querySelector('.section-segundaria').appendChild(card);
+      document.querySelector('.section-segundaria').appendChild(card);
+    } else {
+      const numeroAleatorio = (Math.random() * 5).toFixed(0);
+      console.log(numeroAleatorio);
+      const card = document.createElement('div');
+      card.innerHTML = `<div class="cards">
+          <div class="img-card">
+            <img src="${capaSociais[numeroAleatorio]}" alt="" />
+          </div>
+          <div class="card-texto-container">
+            <h3 class="titulo-card">
+              ${notfeitas}
+            </h3>
+            <p class="texto-card">
+              ${introducao}
+            </p>
+          </div>
+          <span class="data-card">${data_publicacao}</span>
+        </div>`;
+      document.querySelector('.section-segundaria').appendChild(card);
+    }
   });
 }
